49fd6fcb6f99773ba0ac6c4034cbd9b1ff465451
Suryanath G <121388374+surya08nath@users.noreply.github.com>
2025-06-01 15:50:33 +0530

    turtle race game

--- day19/main.py
+++ day19/main.py
@@ -1,29 +1,35 @@
-from turtle import Turtle,Screen
+from turtle import Turtle, Screen
+import random
 
-tim=Turtle()
-screen=Screen()
+is_race_on = False
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: red, orange, yellow, green, blue, purple:")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_positions = [-70, -40, -10, 20, 50, 80]
+all_turtles = []
 
-def move_forwards():
-    tim.forward(10)
-def move_backwards():
-    tim.backward(10)
-def turn_left():
-    new_heading=tim.heading()+10
-    tim.setheading(new_heading)
-def turn_right():
-    new_heading=tim.heading()-10
-    tim.setheading(new_heading)
-def clear():
-    tim.clear()
-    tim.penup()
-    tim.home()
-    tim.pendown()
+for turtle_index in range(0, 6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.color(colors[turtle_index])
+    new_turtle.goto(x=-230, y=y_positions[turtle_index])
+    all_turtles.append(new_turtle)
 
+if user_bet:
+    is_race_on = True
 
-screen.listen()
-screen.onkey(key="w",fun=move_forwards)
-screen.onkey(key="s",fun=move_backwards)
-screen.onkey(key="d",fun=turn_right)
-screen.onkey(key="a",fun=turn_left)
-screen.onkey(key="c",fun=clear)
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner!")
+
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
+
 screen.exitonclick()
